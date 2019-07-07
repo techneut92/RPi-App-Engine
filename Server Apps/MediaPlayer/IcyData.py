41ccaeb1a38afb4a064c2a71b9fb577e6cb622d0
Dylan Westra <dylanwestra@gmail.com>
2019-07-07 16:16:13 +0200

    .

--- Server Apps/MediaPlayer/IcyData.py
+++ Server Apps/MediaPlayer/IcyData.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 import re
 import struct
-import sys
 try:
     import urllib2
 except ImportError:  # Python 3
     import urllib.request as urllib2
-
+import requests
+from threading import Thread
 
 class IcyData:
     __name = None
@@ -19,11 +19,14 @@ class IcyData:
     __requestUrl = None
     __encoding = 'latin1'
     __headers = None
+    __updateThread = None
 
     def __init__(self, r_url):
         self.__requestUrl = r_url
         self.__detectEncoding()
         self.__getData()
+        self.__updateThread = Thread(target=self.__updateData)
+        self.__updateThread.start()
 
     def __detectEncoding(self):
         r_url = self.__requestUrl.lower()
@@ -31,6 +34,21 @@ class IcyData:
             self.__encoding = 'iso-8859-1'
         elif 'ogg' in r_url:
             self.__encoding = 'utf-8'
+
+    def __updateData(self):
+        while True:
+            # new request
+            response = requests.get(self.__requestUrl, headers={'Icy-MetaData': 1}, stream=True)
+            response.raise_for_status()
+            headers, stream = response.headers, response.raw
+            meta_int = headers.get('icy-metaint')
+            audio_length = int(meta_int)
+            audio_data = stream.read(audio_length)
+            meta_byte = stream.read(1)
+            if meta_byte:
+                meta_length = ord(meta_byte) * 16
+                meta_data = stream.read(meta_length)
+                print(meta_data)
 
     def __getData(self):
         request = urllib2.Request(self.__requestUrl, headers={'Icy-MetaData': 1})  # request metadata
@@ -41,7 +59,6 @@ class IcyData:
             response.read(self.__metaint)  # skip to metadata
             metadata_length = struct.unpack('B', response.read(1))[0] * 16  # length byte
             metadata = response.read(metadata_length).rstrip(b'\0')
-            print(str(metadata), file=sys.stderr)
             # extract title from the metadata
             m = re.search(br"StreamTitle='([^']*)';", metadata)
             if m:
